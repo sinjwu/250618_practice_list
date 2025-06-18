2c52d4427ea2fb6ce0b57d28cc0c98d9d425c811
sinjwu <harbirdouk@gmail.com>
2025-06-18 10:07:04 +0900

    250618 practice list

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<String> fruits = new ArrayList<String>();
 
-        fruits.add("Apple");
-        fruits.add("Banana");
-        fruits.add("Cherry");
-        fruits.add("DragonFruit");
         fruits.add("Mango");
+        fruits.add("Bbanana");
+        fruits.add("DragonFruit");
+        fruits.add("Apple");
+        fruits.add("Cherry");
+        fruits.add("Banana");
 
-        for(int i = 0; i < fruits.size(); i++) {
-            System.out.println(fruits.get(i));
+        if(fruits.contains("Apple")) {
+            System.out.println("Apple은 포함되어 있습니다.");
         }
+        Collections.sort(fruits);
+        //for(int i = 0; i < fruits.size(); i++) {
+        //    System.out.println(fruits.get(i));
         for(String fruit: fruits) {
             System.out.println(fruit);
         }
